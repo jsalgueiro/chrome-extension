8beb02b381ed81662161eeee65e327e64d58361e
Jose Salgueiro <jsalgueiro@adsensor.com>
2022-12-04 22:35:23 +0000

    Fixing manifest and creates a sample that adds a button to the rendered page

---
+++ public/content-script.js
@@ -0,0 +1,7 @@
+var greeting = "My new button";
+const button = document.body.appendChild(document.createElement("button"));
+button.innerHTML = greeting;
+button.style.left="50px";
+button.style.top="50px";
+button.style.position="absolute";
+
