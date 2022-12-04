a935b52ac51333e19f9a0ccce1ea65cde5f7ecb1
Jose Salgueiro <jsalgueiro@adsensor.com>
2022-12-04 21:52:54 +0000

    Adding scss to the project. Adding bmac qrcode and link

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+  const bmacURL = 'https://www.buymeacoffee.com/jsalgueiro';
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={'bmc_qr.png'}
+          className="qr-code"
+          alt={`BuyMeACoffee Jose Salgueiro ${bmacURL}`}
+          onClick={() => window.open(bmacURL, '_blank')} />
       </header>
     </div>
   );
